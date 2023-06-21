095cf40a0736adc7f451dc8c6697abf9c93794a0
Anatoliy Matsakov <86084184+WipeRrr@users.noreply.github.com>
2023-06-21 13:50:00 +0000

    form add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
+// import ContactForm from './ContactForm';
+// import ContactList from './ContactList';
+// import Filter from './Filter';
+import Home from "pages/Home";
+import { AppBar } from "./AppBar/AppBar";
+import { Route, Routes } from 'react-router-dom';
+import Register from "pages/Register";
+import Login from "pages/Login";
+import Contacts from 'pages/Contacts';
+
 
 export function App() {
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm></ContactForm>
-      <h2>Contacts</h2>
-      <Filter></Filter>
-      <ContactList></ContactList>
-    </div>
+    <>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/contacts" element={<Contacts />}></Route>
+      </Routes>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store'
 // import { PersistGate } from 'redux-persist/integration/react';
-
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
