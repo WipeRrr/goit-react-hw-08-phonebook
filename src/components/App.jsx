5e2eafaef94fb98d7fc3e2bf85f6722ccd177f35
Anatoliy Matsakov <86084184+WipeRrr@users.noreply.github.com>
2023-06-23 10:13:40 +0000

    presist add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
-// import ContactForm from './ContactForm';
-// import ContactList from './ContactList';
-// import Filter from './Filter';
-import Home from "pages/Home";
-import { AppBar } from "./AppBar/AppBar";
+// import { useAuth } from 'hooks';
+import Home from 'pages/Home';
+import { AppBar } from './AppBar/AppBar';
 import { Route, Routes } from 'react-router-dom';
-import Register from "pages/Register";
-import Login from "pages/Login";
+import Register from 'pages/Register';
+import Login from 'pages/Login';
 import Contacts from 'pages/Contacts';
-
+import { refreshUser } from 'Redux/auth/operations';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 export function App() {
+  const dispatch = useDispatch();
+  // const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <>
       <AppBar />
