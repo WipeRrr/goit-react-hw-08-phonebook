3811070571046178479a7d1280205055773c3b8d
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-06-24 17:22:54 +0300

    del svg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,9 +16,9 @@ export default function Home() {
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>
-        Phonebook welcome page{' '}
+        Phonebook welcome page
         <span role="img" aria-label="Greeting icon">
-          💁‍♀️
+          
         </span>
       </h1>
     </div>
